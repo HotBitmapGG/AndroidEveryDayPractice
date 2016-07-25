13874a736dd7a852b5a113ca4c558b59b55da299
hotshotGG <100332338@qq.com>
2016-07-25 22:15:43 +0800

    HotBitmapGG
    add

---
+++ app/src/main/java/io/netopen/hotbitmapgg/androideverydaypractice/widget/other/MemoryClearView.java
@@ -0,0 +1,31 @@
+package io.netopen.hotbitmapgg.androideverydaypractice.widget.other;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by hcc on 16/7/24 12:19
+ * 100332338@qq.com
+ */
+public class MemoryClearView extends View
+{
+
+    public MemoryClearView(Context context, AttributeSet attrs)
+    {
+
+        super(context, attrs);
+    }
+
+    public MemoryClearView(Context context)
+    {
+
+        super(context);
+    }
+
+    public MemoryClearView(Context context, AttributeSet attrs, int defStyleAttr)
+    {
+
+        super(context, attrs, defStyleAttr);
+    }
+}
